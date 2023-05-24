f6803210935cafd595172e08158660daa3f2fd46
cloudtestingph2 <109664751+cloudtestingph2@users.noreply.github.com>
2023-05-24 18:35:18 +0530

    Update Add_Nums.java

--- Add_Nums.java
+++ Add_Nums.java
@@ -1,2 +1,9 @@
-AddNums.java - file
+public class AddNums {
+   public static void main(String[] args) {
+      System.out.println(add_Nums(1,2));
+   }
 
+  public static int add_Nums(int a, int b) {
+  return a+b;
+  }
+}
