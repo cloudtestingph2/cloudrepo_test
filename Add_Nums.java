0b3e0414d99006069e865c5a336acd173981653a
cloudtestingph2 <109664751+cloudtestingph2@users.noreply.github.com>
2023-05-25 19:06:03 +0530

    Delete Add_Nums.java

--- Add_Nums.java
+++
@@ -1,9 +0,0 @@
-public class AddNums {
-   public static void main(String[] args) {
-      System.out.println(add_Nums(1,2));
-   }
-
-  public static int add_Nums(int a, int b) {
-  return a+b;
-  }
-}
